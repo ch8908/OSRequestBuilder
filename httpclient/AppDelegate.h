debf74041073796bdb16dfd159650fdcc2b2a8e1
Kros <ch8908@gmail.com>
2015-04-09 17:31:32 +0800

    Initial Commit

---
+++ httpclient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  httpclient
+//
+//  Created by Kros Huang on 4/9/15.
+//  Copyright (c) 2015 oSolve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ httpclient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  httpclient
+//
+//  Created by Kros Huang on 4/9/15.
+//  Copyright (c) 2015 oSolve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
